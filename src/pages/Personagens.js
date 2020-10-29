c9b0ad19ba79e7fc092d51d976e7f7974d7f368e
brennop <brennopcordeiro@gmail.com>
2020-10-29 14:07:00 -0300

    😔 let go of all the fear

--- src/pages/Personagens.js
+++ src/pages/Personagens.js
@@ -1,5 +1,16 @@
+import { useEffect, useState } from "react";
 import PersonagemList from "../components/PersonagemList";
+import axios from "axios";
 
-export default function Personagens() {
-  return <PersonagemList personagens={[]} />;
+export default function Personagens({ page }) {
+  const [personagens, setPersonagens] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => response.data)
+      .then((data) => setPersonagens(data.results));
+  }, []);
+
+  return <PersonagemList personagens={personagens} />;
 }
